a6bb96a028b7c78a289d43ddca50e6227c5f9887
SUSH9391 <masrbaliga@gmail.com>
2025-07-20 17:42:04 +0530

    updated app.jsx adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Home from './components/Home.jsx'
+import Footer from './components/Footer.jsx'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
