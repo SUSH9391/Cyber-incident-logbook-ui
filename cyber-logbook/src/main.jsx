ebac39accb2955cf4bc0340e758129ec24246aed
SUSH9391 <masrbaliga@gmail.com>
2025-07-20 16:33:42 +0530

    repaired service worker for https and http

--- cyber-logbook/src/main.jsx
+++ cyber-logbook/src/main.jsx
@@ -3,23 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Register service worker
-if (
-  'serviceWorker' in navigator &&
-  (window.location.protocol === 'https:' || window.location.hostname === 'localhost')
-) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        console.log('SW registered: ', registration);
-      })
-      .catch((registrationError) => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
-} else {
-  console.info('Service Worker not registered — not HTTPS or localhost.');
-}
 
 
 createRoot(document.getElementById('root')).render(
