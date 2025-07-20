f1544752b121cd0da8456a4381da89cbfdc8dbeb
SUSH9391 <masrbaliga@gmail.com>
2025-07-20 16:50:22 +0530

    added all files from sub-folder to main which helps in deployment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Home from './components/Home.jsx'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
